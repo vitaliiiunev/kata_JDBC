968ed7ed1e1b670c8c8df41c40b9b42e815ce349
Vitalii Yunev <vetale4ik@gmail.com>
2024-06-26 13:29:05 +0300

    HomeWork JDBC

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,30 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static String URL = "jdbc:mysql://localhost:3306/new_kata";
+    private static String USER = "root";
+    private static String PASSWORD = "Typokljob09tyt";
+
+    public Util() {
+    }
+
+    public static Connection open () {
+
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+             connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
 }
